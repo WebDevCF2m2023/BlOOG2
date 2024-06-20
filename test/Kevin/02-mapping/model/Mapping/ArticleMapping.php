c0ab9ca42c2b732b7760329d41660e81ffd0e420
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-06-20 15:43:19 +0200

    ArticleMapping

---
+++ test/Kevin/02-mapping/model/Mapping/ArticleMapping.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace model\Mapping;
+
+use model\Abstract\AbstractMapping;
+use model\Trait\TraitDateTime;
+use model\Trait\TraitSlugify;
+use DateTime;
+use Exception;
+
+class ArticleMapping extends AbstractMapping
+{
+    protected ?int $article_id;
+    protected ?string $article_title;
+    protected ?string $article_slug;
+    protected ?string $article_text;
+    protected null|string|DateTime $article_date_create;
+    protected null|string|DateTime $article_date_update;
+    protected null|string|DateTime $article_date_publish;
+    protected bool $article_is_published = false;
+    protected ?int $user_user_id;
+
+    use TraitDateTime;
+    use TraitSlugify;
+
+    public function getArticleDateCreate(): null|string|DateTime
+    {
+        return $this->article_date_create;
+    }
+
+    public function setArticleDateCreate(null|string|DateTime $article_date_create): void
+    {
+        $this->formatDateTime($article_date_create, "article_date_create");
+    }
+
+    public function getArticleDateUpdate(): null|string|DateTime
+    {
+        return $this->article_date_update;
+    }
+
+    public function setArticleDateUpdate(null|string|DateTime $article_date_update): void
+    {
+        $this->formatDateTime($article_date_update, "article_date_update");
+    }
+
+    public function getArticleDatePublish(): null|string|DateTime
+    {
+        return $this->article_date_publish;
+    }
+
+    public function setArticleDatePublish(null|string|DateTime $article_date_publish): void
+    {
+        $this->formatDateTime($article_date_publish, "article_date_publish");
+    }
+
+    public function getUserUserId(): ?int
+    {
+        return $this->user_user_id;
+    }
+
+    public function setUser_user_id(?int $user_user_id): void
+    {
+        $this->user_user_id = $user_user_id;
+    }
+
+    public function getArticleIsPublished(): bool
+    {
+        return $this->article_is_published;
+    }
+
+    public function setArticleIsPublished(bool $article_is_published): void
+    {
+        $this->article_is_published = $article_is_published;
+    }
+
+    public function getArticleTitle(): ?string
+    {
+        return html_entity_decode($this->article_title);
+    }
+
+    public function setArticleTitle(string $article_title): void
+    {
+        $article_title = htmlspecialchars(trim(strip_tags($article_title)),ENT_QUOTES);
+        $this->article_title = $article_title;
+    }
+
+    public function getArticleSlug(): ?string
+    {
+        return $this->article_slug;
+    }
+
+    public function setArticleSlug(?string $article_slug): void
+    {
+        $this->article_slug = $this->slugify($article_slug === null ? $this->getArticleTitle() : $article_slug);
+    }
+
+    public function getArticleId(): ?int
+    {
+        return $this->article_id;
+    }
+
+    /**
+     * @param $article_id must be equal or greater than 0
+     * @throws Invalide $article_id
+     */
+    public function setArticleId(?int $article_id): void
+    {
+        if($article_id<=0 && $article_id !== null) throw new Exception("ID non valide"); 
+        $this->article_id = $article_id;
+    }
+
+    public function getArticleText(): ?string
+    {
+        return $this->article_text;
+    }
+
+    public function setArticleText(string $article_text): void
+    {
+        $this->article_text = $article_text;
+    }
+}
