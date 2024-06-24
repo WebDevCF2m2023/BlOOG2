552ecb8eb1961ac32f8fa1375d6ea3c7290a7d32
mikhawa <michaeljpitz@gmail.com>
2024-06-24 10:03:42 +0200

    index

--- public/index.php
+++ public/index.php
@@ -15,31 +15,7 @@ spl_autoload_register(function ($class) {
     require PROJECT_DIRECTORY.'/' .$class . '.php';
 });
 
-// on peu utilisé l'ecritire longue pour appeler une classe
-// via
-$tag1 = new \model\Mapping\MappingTag([
-    'tag_id' => 8,
-    'tag_slag' => "php-8.4",
-    "Je m'amuse beaucoup!"
-]);
+// our router
 
-//maniére courte l'alias n'est créé que si on a 2 classes
-//qui portent le meme nom
-use Model\Mapping\MappingTag as lulu;
-    
-$tag2 = new MappingTag([
-    'tag_id' => 13,
-    'tag_slag' => "php-9",
-    'Arggg_Ho_Tempo' => "12453"
-]);
-
-
-$tag2 = new MappingTag([
-    'tag_id'=>15,
-    'tag_slug'=> "J'aimerais que ceci soit un slug",
-]);
-
-var_dump($tag1)($tag2);
-
-
-//exemple : $test = newClasse();
+?>
+<h1>Accueil</h1>
