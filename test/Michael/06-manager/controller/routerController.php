4e2abe85c919a1b545e69166e32001489c988d8f
mikhawa <michaeljpitz@gmail.com>
2024-07-02 15:17:15 +0200

    CRUD à faire

--- test/Michael/06-manager/controller/routerController.php
+++ test/Michael/06-manager/controller/routerController.php
@@ -7,26 +7,40 @@ $route = $_GET['route'] ?? 'home';
 switch ($route) {
 
     case 'home':
+        # affichage de la page d'accueil (vue)
         include_once PROJECT_DIRECTORY.'/view/homepage.view.php';
         break;
     case 'article':
-
+        # todo
+        echo 'article à gérer';
         break;
     case 'category':
-
+        echo 'category à gérer';
         break;
+    # controller déjà présent
     case 'comment':
+        # c'est lui qui va gérer les actions sur les commentaires
+        # et afficher les vues associées
         require 'commentController.php';
         break;
     case 'permission':
-
+        # todo
+        echo 'permission à gérer';
         break;
     case 'user':
+        # todo
+        echo 'user à gérer';
+        break;
     case 'file':
+        # todo
+        echo 'file à gérer';
+        break;
     case 'tag':
-
+        # todo
+        echo 'tag à gérer';
         break;
     default:
-        require '../view/404.view.php';
+        # affichage de la page 404
+        require PROJECT_DIRECTORY.'/view/404.view.php';
         break;
 }
