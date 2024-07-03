6f09d191940e139dcebea48f1bce029dd27befe0
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-07-03 14:38:16 +0200

    update default option

--- test/Gregory/06-manager/view/comment/updateComment.view.php
+++ test/Gregory/06-manager/view/comment/updateComment.view.php
@@ -24,12 +24,12 @@
         <label for="comment_text">Commentaire</label>
         <select name="user_user_id" id="user_user_id">
             <?php foreach($users as $user): ?>
-                <option value="<?=$user['user_id']?>"><?=$user["user_login"]?></option>
+                <option value="<?=$user['user_id']?>" <?=$selectOneComment->getUserUserId()==$user["user_id"] ? "selected" : ""?>><?=$user["user_login"]?></option>
             <?php endforeach; ?>
         </select>
         <select name="article_article_id" id="article_article_id">
             <?php foreach($articles as $article): ?>
-                <option value="<?=$article['article_id']?>"><?=$article["article_title"]?></option>
+                <option value="<?=$article['article_id']?>" <?=$selectOneComment->getArticleArticleId()==$article["article_id"] ? "selected" : ""?>><?=$article["article_title"]?></option>
             <?php endforeach; ?>
         </select>
         <textarea name="comment_text" id="comment_text" cols="30" rows="10"><?=$selectOneComment->getCommentText()?></textarea>
