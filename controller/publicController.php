7ed3b01a4083fcf007f2dee4b8010d4febd24cb1
mikhawa <michaeljpitz@gmail.com>
2024-07-04 12:52:40 +0200

    test menu

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,4 +1,11 @@
 <?php
+use model\Manager\CategoryManager;
+
+$categoryManager = new CategoryManager($db);
+
+$categories = $categoryManager->selectAllForMenu();
+
+var_dump($categories);
 
 // exemple template
 echo $twig->render('publicView/public.homepage.html.twig');
